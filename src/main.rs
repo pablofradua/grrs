f800c932621113054720c96226e1a397cd67591a
Pablo Fradua <pablo.fradua@gmail.com>
2021-08-09 23:33:57 +0200

    Added some documentation to the Cli struct

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use structopt::StructOpt;
-fn main() {
-}
+
+fn main() {}
+
+/// Search for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt)]
 struct Cli {
+    /// The pattern to look for
     pattern: String,
+    /// The path to the file to read
     path: std::path::PathBuf,
 }
